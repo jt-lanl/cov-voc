c5be9a1d200987636b93840d2bab7ad45022b184
jt <jt@lanl.gov>
2021-05-04 07:54:43 -0600

    required by readseq

---
+++ seqsample.py
@@ -0,0 +1,13 @@
+class SequenceSample:
+    def __init__(self,name,seq):
+        self.name = name
+        self.seq = seq
+
+    def __eq__(self,other):
+        return self.seq == other.seq and self.name == other.name
+
+    def copy(self):
+        return type(self)(self.name,self.seq)
+
+    def deepcopy(self):
+        return type(self)(self.name,self.seq)
