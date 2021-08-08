290a88e75b90d7b0159dcb3c3d7472703cc3ef22
jt <jt@lanl.gov>
2021-08-08 16:59:52 -0600

    New code to handle insertions

---
+++ mutantx.py
@@ -0,0 +1,351 @@
+'''
+mutantx: this version does insertions
+
+Mutation and SingleSiteMutation classes,
+where Mutation is a list of SingleSiteMutations, with auxiliary information such as an 'exact' flag.
+A SingleSiteMutation has three components:
+  site = integer site at which mutation occurs
+  ref  = character amino acid at that site in reference sequence
+         + indicates insertion after that site
+  mut  = character amino acid at that site in mutated sequence
+         . indicates any character
+         _ indicates ref
+         * indicates any character except ref
+         multicharacter strings indicate either:
+           if ref == +, then multiple characters are inserted
+           otherwise, indicates any character in the multicharacter string
+
+Note that there are two kinds of mutantion strings: prescriptive and descriptive
+Prescriptive: allows us create a mutation sequence from a reference sequence
+Descriptive: we can assess whether a sequence is described by the mutation
+
+eg, [A222V,D614G] is prescriptive (and being prescriptive, is also descriptive)
+and [A222*,D614G] is not prescriptive, but is descriptive
+    [+222FF] indicates that "FF" is inserted after site 222; this is prescriptive
+    [A222DG] indicates that A mutates to either D or G
+
+'''
+
+import sys
+import re
+import itertools as it
+import warnings
+
+class SiteIndexTranslator():
+    '''provide functions to translate between string index and site number
+       refseq: ABC--D
+       site:   123334
+       index:  012345
+    '''
+    def __init__(self,refseq):
+        # self.refseq = refseq ## needed?
+        self.site = list(it.accumulate(int(b!='-') for b in refseq))
+        self.ndx = [None]*(1+max(self.site))
+        for n,p in enumerate(self.site):
+            if self.ndx[p] is None:
+                self.ndx[p] = n
+
+    def site_from_index(self,n):
+        '''return site number associated with index'''
+        return self.site[n]
+
+    def index_from_site(self,site):
+        '''return index associated with site number'''
+        return self.ndx[site]
+
+    def indices_from_site(self,site):
+        ''' return range of indices for a single site '''
+        return range(self.ndx[site],self.ndx[site+1]) ## but what if site is the last site?
+
+    def indices_from_sitelist(self,sitelist):
+        '''return list indices from list of sites'''
+        ndxlist = []
+        for site in sitelist:
+            ndxlist.extend( self.indices_from_site(site) )
+        return ndxlist
+
+class SingleSiteMutation():
+    ''' eg, D614G is a single site mutation '''
+    def __init__(self,mstring):
+        mstring = mstring.strip()
+        m = re.match(r"(.)(\d+)(.[A-Z-_]*)",mstring)
+        if not m:
+            raise RuntimeError(f"Mutation string /{mstring}/ invalid")
+        self.mstring = mstring
+        self.ref = m[1]
+        self.site = int(m[2])
+        self.mut = m[3]
+
+        if self.mut == self.ref:
+            print(self,self.mut,"==",self.ref,file=sys.stderr)
+
+        if "_" in self.mut:
+            self.mut = re.sub("_",self.ref,self.mut)
+
+    def __eq__(self,other):
+        return self.ref == other.ref and self.site == other.site and self.mut == other.mut
+
+    def __hash__(self):
+        ''' enables the making of sets of SSM's '''
+        return hash(tuple((self.ref,self.site,self.mut)))
+
+    def __str__(self):
+        return self.mstring
+
+class Mutation(list):
+    ''' Mutation is a list of SingleSiteMutations '''
+
+    @classmethod
+    def merge(cls,*muts):
+        '''merge two or more mutations into a single mutation'''
+        mergedmut = set()
+        for mut in muts:
+            mergedmut.update(mut)
+        mergedmut = cls(list(mergedmut))
+        return mergedmut.sort()
+
+    def __init__(self,ssms=None):
+        super().__init__(self)
+        try:
+            ## default translators (will be replaced when refseq is specified)
+            self.site_from_index = lambda ndx: ndx+1
+            self.index_from_site = lambda pos: pos-1
+
+            ## initialization according to what ssms is
+            if isinstance(ssms,list):
+                self.extend(ssms)
+            elif isinstance(ssms,str):
+                self.init_from_line(ssms)
+            elif isinstance(ssms,tuple):
+                self.init_from_sequences(*ssms)
+            else:
+                ## just initialize as empty list
+                pass
+        except Exception as exception:
+            raise RuntimeError(f"Unable to initialze with ssms={ssms}") from exception
+
+    def init_from_line(self,line):
+        ''' initialize Mutation from mstring '''
+        mat = re.match(r".*\[(.*)\](!?).*",line)
+        if mat:
+            self.exact = bool(mat[2])
+            for m in mat[1].split(","):
+                if m.strip():
+                    self.append( SingleSiteMutation(m) )
+        return self
+
+    def init_from_sequences(self,refseq,seq,exact=False):
+        ''' find all sites where seq differs from refseq,
+            convert into a Mutation
+        '''
+        ## note multisite insertions are not yet supported
+        self.exact = exact
+        self.set_reference_sequence(refseq)
+        muts = []
+        for n,(r,c) in enumerate(zip(refseq,seq)):
+            if c != r:
+                site = self.site_from_index(n)
+                muts.append(SingleSiteMutation(f"{r}{site}{c}"))
+
+        def flush_ssmlist(ssmlist):
+            allsites = [t.site for t in ssmlist]
+            if not all(s == allsites[0] for s in allsites):
+                raise RuntimeError(f"assertion failed {allsites} not all equal")
+            site = ssmlist[-1].site
+            mstr = "".join(t.mut for t in ssmlist)
+            self.append(SingleSiteMutation(f"+{site}{mstr}"))
+            ssmlist.clear()
+
+        ## Merge [-67A,-67B,-67C] -> [+67ABC]
+        tmp_ssmlist=[]
+        for ssm in muts:
+            if ssm.ref == '-':
+                if tmp_ssmlist and tmp_ssmlist[-1].site != ssm.site:
+                    flush_ssmlist(tmp_ssmlist)
+                    #tmp_ssmlist=[]
+                tmp_ssmlist.append(ssm)
+            else:
+                if tmp_ssmlist:
+                    flush_ssmlist(tmp_ssmlist)
+                    #tmp_ssmlist=[]
+                self.append(ssm)
+        if tmp_ssmlist:
+            flush_ssmlist(tmp_ssmlist)
+            #tmp_ssmlist=[]
+
+        return self
+
+    def set_reference_sequence(self,refseq):
+        '''use refseq to set up site_from_index and index_from_site methods'''
+        T = SiteIndexTranslator(refseq)
+        self.site_from_index = T.site_from_index
+        self.index_from_site = T.index_from_site
+        return self
+
+    def sort(self):
+        '''return a sorted copy off self'''
+        return type(self)(sorted(self,key=lambda m: m.site))
+
+    def inconsistent(self):
+        ''' return an inconsistent pair of ssm's if there is one '''
+        ## inconsistent if site is the same but mutation is different
+        ## [T95I] and [G142D] are consistent because sites are different
+        ## [T95I] and [T95T] are inconsistent, but
+        ## [T95I] and [T95.] are unequal but consistent
+        ## don't (yet) handle [T95_] as synonym for [T95T]
+        for ssma in self:
+            for ssmb in self:
+                if ssma.site != ssmb.site:
+                    continue
+                if ssma.ref != ssmb.ref:
+                    ## not only inconsistent, but one of then doesn't match refseq
+                    return (str(ssma),str(ssmb))
+                if ssma.mut == "." or ssmb.mut == ".":
+                    continue
+                if ssma.mut == "*" or ssmb.mut == "*":
+                    continue
+                if ssma.mut != ssmb.mut:
+                    return (str(ssma),str(ssmb))
+        return None
+
+    def check(self):
+        ''' check consistency '''
+        return not bool(self.inconsistent())
+
+    def checkref(self,refseq):
+        ''' return True if ssm.ref = refseq at all mutation sites '''
+        self.set_reference_sequence(refseq)
+        check=True
+        for ssm in self:
+            ndx = self.index_from_site(ssm.site)
+            if ssm.ref == '+':
+                continue
+            if ssm.ref != refseq[ndx]:
+                warnings.warn(f"Mismatch for {ssm}: {ssm.ref} != {refseq[ndx]}")
+                check=False
+        return check
+
+    def pattern(self,refseq,exact=False):
+        '''sort of a regex-pattern, but lite'''
+        if exact is not False:
+            ## we ignore 'exact'
+            raise RuntimeError("mutantx.Mutation().pattern exact")
+        self.set_reference_sequence(refseq)
+        mutseq = list(refseq)
+        if not self.exact:
+            mutseq = [c if c=="-" else "." for c in refseq]
+        for ssm in self:
+            ndx = self.index_from_site(ssm.site)
+            if ssm.ref == '+':
+                mutseq[ndx] += ssm.mut
+                for n in range(ndx+1,ndx+1+len(ssm.mut)):
+                    if mutseq[n] == '-':
+                        mutseq[n] = ""
+            elif len(ssm.mut) > 1:
+                mutseq[ndx] = "&"
+            else:
+                mutseq[ndx] = ssm.mut
+        mutseq = "".join(mutseq)
+        return mutseq
+
+    def regex_pattern(self,refseq,exact=None,nodash=False):
+        '''return pattern that can be used as regex to search for mutation'''
+        ## exact: needs to match refseq at all sites not in mutation
+        ## otherwise: only needs to match at mutation sites
+        if exact is None:
+            exact = self.exact
+
+        pattern = list(refseq)
+        if not exact:
+            ## all .'s except -'s where the -'s are in refseq
+            pattern = ['.' if r != '-' else '-' for r in refseq]
+
+        for ssm in self:
+            ndx = self.index_from_site(ssm.site)
+            if ssm.ref == "+":
+                pattern[ndx] += ssm.mut
+                for n in range(ndx+1,ndx+1+len(ssm.mut)):
+                    if exact:
+                        assert pattern[n] == '-'
+                    pattern[n] = ""
+            elif ssm.mut == "*":
+                pattern[ndx] = "[^"+ssm.ref+"]"
+            elif ssm.mut == "_":
+                pattern[ndx] = ssm.ref
+            elif len(ssm.mut) > 1:
+                pattern[ndx] = "[" + ssm.mut + "]"
+            else:
+                try:
+                    pattern[ndx] = ssm.mut
+                except IndexError as exception:
+                    print("pattern=",pattern)
+                    print("ndx=",ndx)
+                    print("ssm:",ssm)
+                    raise IndexError from exception
+
+        pattern = "".join(pattern)
+        if nodash:
+            pattern = re.sub("-","",pattern)
+        return pattern
+
+    def mutate_sequence(self,refseq,prescriptive=True):
+        '''return sequence that is mutated version of refseq'''
+        self.set_reference_sequence(refseq)
+        mutseq = list(refseq)
+        for ssm in self:
+            ndx = self.index_from_site(ssm.site)
+            if ssm.ref == '+':
+                mutseq[ndx] += ssm.mut
+                for n in range(ndx+1,ndx+1+len(ssm.mut)):
+                    if mutseq[n] == '-':
+                        mutseq[n] = ""
+            elif ssm.mut == ".":
+                if prescriptive:
+                    warnings.warn(f"{ssm} is not prescriptive, using {ssm.ref}")
+                    mutseq[ndx] = ssm.ref
+                else:
+                    mutseq[ndx] = "."
+            elif ssm.mut == "*":
+                if prescriptive:
+                    warnings.warn(f"{ssm} is not prescriptive, using X")
+                    mutseq[ndx] = "X"
+                else:
+                    mutseq[ndx] = "*"
+            elif len(ssm.mut) > 1:
+                if prescriptive:
+                    warnings.warn(f"{ssm} is not prescriptive, using {ssm.mut[0]}")
+                    mutseq[ndx] = ssm.mut[0]
+                else:
+                    mutseq[ndx] = "&"
+            else:
+                mutseq[ndx] = ssm.mut
+        mutseq = "".join(mutseq)
+        return mutseq
+
+    def __str__(self):
+        return "[" + ",".join(str(ssm) for ssm in self) + "]" + ("!" if self.exact else "")
+
+if __name__ == "__main__":
+
+    ma = Mutation("[S13S,S494P,N501N,D614G,P681H,T716I,S982S,D1118D]")
+    mb = Mutation().init_from_line("[S13S,L141-,G142-,V143-,A222A,L452R,D614G,T859T,D950D]")
+    mc = Mutation.merge(ma,mb)
+    print("A:",ma)
+    print("B:",mb)
+    print("C inconsistent:",mc.inconsistent())
+    print("C:",mc.check(),mc)
+    print("C:",mc.sort())
+
+    RefSeq = "ABC-D--EFG"
+    for newseq in ["ABC-E--EFG",
+                   "ABCWD--EFH",
+                   "BBC-E--EFF",
+                   "BBC-E-WEFF",
+                   "BBC-EW-EFF",
+                   "ABCWEXYEFG",
+                   "ABCWDXYEFG",
+    ]:
+        mu = Mutation().init_from_sequences(RefSeq,newseq)
+        rpatt = mu.regex_pattern(RefSeq)
+        rpattx = mu.regex_pattern(RefSeq,exact=True)
+        print(RefSeq,newseq,rpatt,rpattx,mu)
---
+++ pangocommonformsx.py
@@ -0,0 +1,179 @@
+'''
+From sequence file, obtain all the pango lineages, and for each one,
+find the mutation string that corresponds to the consensus sequence
+for that lineage, and show the most commmon forms.
+'''
+import sys
+import re
+from collections import Counter,defaultdict
+import argparse
+
+import sequtil
+import covid
+import mutantx as mutant
+import wrapgen
+from hamming import hamming
+
+def getargs():
+    '''get arguments from command line'''
+    ap = argparse.ArgumentParser(description=__doc__)
+    paa = ap.add_argument
+    covid.corona_args(ap)
+    paa("--npatterns","-n",type=int,default=0,
+        help="How many of the most common patterns per lineage (0=all)")
+    paa("--mincount","-m",type=int,default=10,
+        help="Show only patters with at least this many counts")
+    paa("--consensusalways","-c",action="store_true",
+        help="Always show consensus, even if not common")
+    paa("--verbose","-v",action="count",default=0,
+        help="verbose")
+    args = ap.parse_args()
+    return args
+
+def get_matches(mutantstring,seqlist,fullmatch=False):
+    '''return sequences in seqlist that match the mutantstring'''
+
+    firstseq = seqlist[0].seq
+    muts = mutant.Mutation().init_from_line(mutantstring)
+    assert muts.checkref(firstseq,verbose=True)
+    matchpatt = muts.regex_pattern(firstseq,exact=bool(fullmatch))
+    rematchpatt = re.compile(matchpatt)
+
+    seqmatches = [s for s in seqlist[1:]
+                  if rematchpatt.match(s.seq)]
+
+    return seqmatches
+
+def get_lineage_from_name(name):
+    return re.sub(r".*EPI_ISL_\d+\.","",name)
+
+def count_lineages(seqlist):
+    '''return a counter with number of sequences for each lineage'''
+    lineages = [get_lineage_from_name(s.name) for s in seqlist]
+    return Counter(lineages)
+
+def format_counter(cnt):
+    '''make a nicely formatted string summarizing contents of counter'''
+    total = sum(cnt.values())
+    scnt = sorted(cnt,key=cnt.get,reverse=True)
+    s="%6d %3d " % (total,len(cnt))
+    for lin in scnt[:3]:
+        s += "%6d %5.1f%% %10s; " % (cnt[lin],100*cnt[lin]/total,lin)
+    return s
+
+def mostcommonchar(clist):
+    '''return the most common item in the list'''
+    [(c,_)] = Counter(clist).most_common(1)
+    return c
+
+def consensus(seqlist):
+    '''create a consesnsus sequence from the sequence list'''
+    return "".join(mostcommonchar(clist)
+                   for clist in sequtil.gen_columns_seqlist(seqlist))
+
+def main(args):
+
+    print("COMMON FORMS OF SPIKE WITH A GIVEN PANGO LINEAGE DESIGNATION")
+    print()
+    print("For each lineage, we show the most common forms of Spike, "
+          "as well as their counts within (and percentages of) the lineage.  "
+          "[Note that if a lineage contains several divergent forms of Spike, "
+          "the consensus form might not be found among these common forms.] "
+          "Also shown is the Hamming distance (HD) between each form "
+          "and the most common form in that lineage."
+          "")
+    print()
+
+    count_forms = f" {args.npatterns}" if args.npatterns else ""
+    min_count = f" that have at least {args.mincount} counts (but we always show the most common form)" if args.mincount else ""
+    consensus_always = f"And we always show the consensus form. " if args.consensusalways else ""
+    print(f"We show the{count_forms} most common forms{min_count}. {consensus_always}")
+
+    seqs = covid.read_filter_seqfile(args)
+    seqlist = list(seqs)
+
+    last_days = f" in the last {args.days} days from our last update," if args.days else ""
+    print(f"This output is based on sequences sampled{last_days} from %s to %s." \
+          % sequtil.range_of_dates(seqlist))
+
+    firstseq = seqlist[0].seq
+
+    cnt_lin = count_lineages(seqlist[1:])
+    lineages = sorted(cnt_lin,key=cnt_lin.get,reverse=True)
+
+    seqlist_by_lineage=defaultdict(list)
+    for s in seqlist[1:]:
+        lin = get_lineage_from_name(s.name)
+        seqlist_by_lineage[lin].append(s)
+
+    maxlinlen = max(len(lin) for lin in lineages)
+    fmt = "%%-%ds" % (maxlinlen,)
+    fmt_lin = {lin: fmt%lin for lin in lineages}
+    fmt_lin["EMPTY"] = fmt % ("",)
+
+    print()
+    print(fmt % "Pango","Lineage   Form   Form")
+    print(fmt % "Lineage","  Count  Count    Pct  HD [Spike form as mutation string]")
+
+    for lin in lineages:
+
+        if lin == "None":
+            continue
+        
+        seqlin = seqlist_by_lineage[lin]
+
+        ## First get consensus form
+        cons = consensus(seqlin)
+        cntcons = sum(1 for s in seqlin if s.seq == cons)
+        mcons = mutant.Mutation((firstseq,cons))
+
+        ## Now get most common forms
+        cntr = Counter(s.seq for s in seqlin)
+        cflag = False
+        cntrlist = sorted(cntr,key=cntr.get,reverse=True)
+        if args.npatterns:
+            cntrlist = cntrlist[:args.npatterns]
+        for n,comm in enumerate(cntrlist):
+            cnt = cntr[comm]
+            if n>0 and cnt < args.mincount:
+                break
+            cons_string = ""
+            if comm == cons:
+                cflag = True
+                cons_string = "(consensus)"
+            m = mutant.Mutation((firstseq,comm))
+            if n == 0:
+                top_comm = comm
+                h = 0
+                print("%s %7d %6d %5.1f%% %3d %s %s" %
+                      (fmt_lin[lin],cnt_lin[lin],cnt,100*cnt/cnt_lin[lin],
+                       h,str(m),cons_string))
+            else:
+                h = hamming(top_comm,comm)
+                print("%s %7d %6d %5.1f%% %3d %s %s" %
+                      (fmt_lin[lin],cnt_lin[lin],cnt,100*cnt/cnt_lin[lin],
+                       h,str(m),cons_string))
+        if args.consensusalways and not cflag:
+            h = hamming(top_comm,cons)
+            print("%s %7d %6d %5.1f%% %3d %s (consensus)" %
+                  (fmt_lin[lin],cnt_lin[lin],cntcons,100*cntcons/cnt_lin[lin],
+                   h,str(mcons)))
+        print()
+
+if __name__ == "__main__":
+
+    _args = getargs()
+    def vprint(*p,**kw):
+        '''verbose print'''
+        if _args.verbose:
+            print(*p,file=sys.stderr,flush=True,**kw)
+    def vvprint(*p,**kw):
+        '''very verbose print'''
+        if _args.verbose>1:
+            print(*p,file=sys.stderr,flush=True,**kw)
+
+    def vcount(seqs,*p,**kw):
+        '''if verbose, count seqs as they go by'''
+        return wrapgen.keepcount(seqs,*p,**kw) if _args.verbose else seqs
+
+    main(_args)
