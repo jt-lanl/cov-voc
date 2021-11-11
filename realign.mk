d56c7dcf7ce09f16e2e8feb8b6f263f65b7165b0
jt <jt@lanl.gov>
2021-11-10 18:32:56 -0700

    clearer comments

--- realign.mk
+++ realign.mk
@@ -1,8 +1,9 @@
 ## Makefile for boosting the alignment of sequences
-## make -f src/realign.mk makes a realigned sequences from INITALIGN
-## make -f src/realign.mk KEEPX='' turns off --keepx
-## make -j3 -f src/realign.mk permits parallel make
-## make -f src/realign.mk Latest makes a new Latest.ipkl
+## make -f src/realign.mk                  makes re-aligned sequences from INITALIGN
+## make -f src/realign.mk KEEPX=''         turns off --keepx
+## make -j3 -f src/realign.mk              permits parallel make
+## make -j3 -f src/realign.mk ID=20211024  makes re-aligned sequences based on date 20211024
+## make -f src/realign.mk Latest           makes a new Latest.ipkl
 
 ID := 20211108
 INITALIGN := data/REG_COMP.SPIKE.protein.Human.$(ID).fasta.gz
