9319d5ac392592f74e6a2dc4fee682349c85aec2
jt <jt@lanl.gov>
2021-12-09 11:11:14 -0700

    added AY.122 and updated Omicron

--- lineagetable.py
+++ lineagetable.py
@@ -9,14 +9,15 @@ LineageTable=[
     ('Purple', 'Delta_AY.26', r'AY\.26(\..*)?'),
     ('Lavender', 'Delta_AY.33', r'AY\.33(\..*)?'),
     ('SkyBlue', 'Delta_AY.98.1', r'AY\.98\.1(\..*)?'),
-    ('Goldenrod', 'Delta_AY.47', r'AY\.47(\..*)?'),
+    ('Cornsilk', 'Delta_AY.47', r'AY\.47(\..*)?'),
     ('HotPink', 'Delta_AY.35', r'AY\.35(\..*)?'),
     ('CadetBlue', 'Delta_AY.4.2', r'AY\.4\.2(\..*)?'),   
+    ('Tan', 'Delta_AY.122', r'AY\.122(\..*)?'),
     ('BlueViolet', 'Delta', r'(B\.1\.617\.2)|(AY\..*)'), # B.1.617.2 plus all AY.* except those listed above
 
     ('Yellow','C.1.2', r'C\.1\.2(\..*)?'),
     ('Khaki','B.1.1.318', r'B\.1\.1\.318(\..*)?'),
     ('Aqua','B.1.640', r'B\.1\.640(\..*)?'),
-    ('Red','Omicron', r'B\.1\.1\.529'),
+    ('Red','Omicron', r'BA\.[12]'),
 ]
 
