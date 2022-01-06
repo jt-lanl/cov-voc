39722b31d18f67cc83e89d55fb86c99bebacc4a5
jt <jt@lanl.gov>
2022-01-06 09:46:40 -0700

    Updates, including BA.2 variant of Omicron

--- lineagetable.py
+++ lineagetable.py
@@ -1,9 +1,14 @@
 LineageTable=[
     ('Orange', 'Alpha', r'(B\.1\.1\.7)|(Q\..*)'),
+    ('Magenta', 'Lambda', r'C\.37(\..*)?'),
     ('LightPink', 'Beta', r'B\.1\.351(\..*)?'),
-    ('Green', 'Lambda', r'C\.37(\..*)?'),
     ('LimeGreen', 'Mu', r'B\.1\.621(\..*)?'),
     ('FireBrick', 'Gamma', r'P\.1(\..*)?'),
+    ('Cyan', 'Epsilon', r'B\.1\.42[97](\..*)?'),
+    ('DodgerBlue', 'Iota', r'B\.1\.526(\..*)?'),
+    ('Gold', 'Eta', r'B\.1\.525(\..*)?'),
+    ('OliveDrab', 'Kappa', r'B\.1\.617\.1(\..*)?'),
+    ('Goldenrod', 'R.1', r'R\.1(\..*)?'),
     
     ('ForestGreen', 'Delta_AY.25', r'AY\.25(\..*)?'),
     ('Purple', 'Delta_AY.26', r'AY\.26(\..*)?'),
@@ -11,13 +16,14 @@ LineageTable=[
     ('SkyBlue', 'Delta_AY.98.1', r'AY\.98\.1(\..*)?'),
     ('Cornsilk', 'Delta_AY.47', r'AY\.47(\..*)?'),
     ('HotPink', 'Delta_AY.35', r'AY\.35(\..*)?'),
-    ('CadetBlue', 'Delta_AY.4.2', r'AY\.4\.2(\..*)?'),   
-    ('Tan', 'Delta_AY.122', r'AY\.122(\..*)?'),
+    ('CadetBlue', 'Delta_AY.4.2', r'AY\.4\.2'),   
+    ('Tan', 'Delta_AY.4.2.1', r'AY\.4\.2\.1(\..*)?'),
     ('BlueViolet', 'Delta', r'(B\.1\.617\.2)|(AY\..*)'), # B.1.617.2 plus all AY.* except those listed above
 
     ('Yellow','C.1.2', r'C\.1\.2(\..*)?'),
     ('Khaki','B.1.1.318', r'B\.1\.1\.318(\..*)?'),
-    ('Aqua','B.1.640', r'B\.1\.640(\..*)?'),
-    ('Red','Omicron', r'(B\.1\.1\.529)|(BA\.[12])'),
+    ('DarkTurquoise','B.1.640', r'B\.1\.640(\..*)?'),
+    ('Red','Omicron', r'(B\.1\.1\.529)|(BA\.1)'),
+    ('Maroon','Omicron_BA.2',r'BA\.2'),
 ]
 
