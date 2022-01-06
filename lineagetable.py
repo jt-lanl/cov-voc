31b758aeab5ef0c08360bd85225f876a811e917f
jt <jt@lanl.gov>
2022-01-06 10:01:33 -0700

    Further updates: fewer Deltas, more general Omicron definitions

--- lineagetable.py
+++ lineagetable.py
@@ -11,19 +11,19 @@ LineageTable=[
     ('Goldenrod', 'R.1', r'R\.1(\..*)?'),
     
     ('ForestGreen', 'Delta_AY.25', r'AY\.25(\..*)?'),
-    ('Purple', 'Delta_AY.26', r'AY\.26(\..*)?'),
-    ('Lavender', 'Delta_AY.33', r'AY\.33(\..*)?'),
-    ('SkyBlue', 'Delta_AY.98.1', r'AY\.98\.1(\..*)?'),
-    ('Cornsilk', 'Delta_AY.47', r'AY\.47(\..*)?'),
-    ('HotPink', 'Delta_AY.35', r'AY\.35(\..*)?'),
+    #('Purple', 'Delta_AY.26', r'AY\.26(\..*)?'),
+    #('Lavender', 'Delta_AY.33', r'AY\.33(\..*)?'),
+    #('SkyBlue', 'Delta_AY.98.1', r'AY\.98\.1(\..*)?'),
+    #('Cornsilk', 'Delta_AY.47', r'AY\.47(\..*)?'),
+    #('HotPink', 'Delta_AY.35', r'AY\.35(\..*)?'),
     ('CadetBlue', 'Delta_AY.4.2', r'AY\.4\.2'),   
-    ('Tan', 'Delta_AY.4.2.1', r'AY\.4\.2\.1(\..*)?'),
+    #('Tan', 'Delta_AY.4.2.1', r'AY\.4\.2\.1(\..*)?'),
     ('BlueViolet', 'Delta', r'(B\.1\.617\.2)|(AY\..*)'), # B.1.617.2 plus all AY.* except those listed above
 
     ('Yellow','C.1.2', r'C\.1\.2(\..*)?'),
     ('Khaki','B.1.1.318', r'B\.1\.1\.318(\..*)?'),
     ('DarkTurquoise','B.1.640', r'B\.1\.640(\..*)?'),
-    ('Red','Omicron', r'(B\.1\.1\.529)|(BA\.1)'),
-    ('Maroon','Omicron_BA.2',r'BA\.2'),
+    ('Maroon','Omicron_BA.2',r'BA\.2(\..*)?'),
+    ('Red','Omicron', r'(B\.1\.1\.529)|(BA\..*)'),
 ]
 
