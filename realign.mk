f5ee89c64841518296c336ad58e9745d3ec18e38
jt <jt@lanl.gov>
2022-02-06 18:06:27 -0700

    updated tweakfile.txt; default now skips xalign

--- realign.mk
+++ realign.mk
@@ -5,7 +5,7 @@
 ## make -j3 -f src/realign.mk ID=20211024  makes re-aligned sequences based on date 20211024
 ## make -f src/realign.mk Latest           makes a new Latest.ipkl
 
-ID := 20211108
+ID := 20220206
 INITALIGN := data/REG_COMP.SPIKE.protein.Human.$(ID).fasta.gz
 KEEPX := --keepx
 ifeq ($(KEEPX),--keepx)
@@ -15,19 +15,26 @@ XID := $(ID)
 endif
 XIDGZ := $(XID).fasta.gz
 
-.DEFAULT_GOAL := ztkfx-sk-$(XIDGZ)
+.DEFAULT_GOAL := ztkfx-$(XIDGZ)
+#.DEFAULT_GOAL := ztkfx-sk-$(XIDGZ)
 
 tweakfile.txt:
 	echo '[E156G,F157-,R158-] [E156-,F157-,R158G]' > $@
 	echo '[+214AAG,D215Y] [D215A,+215AGY]' >> $@
 	echo '[Y144_,Y145-] [Y144-,Y145_]' >> $@
-	echo '[N211-,L212I] [N211I,L212-]' >> $@
+	echo '[N211I,L212-] [N211-,L212I]' >> $@
+	echo '[L244Y,H245-] [L244-,H245Y]' >> $@
+	echo '[L24S,P25-,P26-,A27-] [L24-,P25-,P26-,A27S]' >> $@
+	echo '[Y248S,L249-,T250-,P251-,G252-,D253-,S254-] [Y248-,L249-,T250-,P251-,G252-,D253-]' >> $@
 
 fx-$(XIDGZ): $(INITALIGN)
 	python -m fixalign $(KEEPX) -i $(INITALIGN) --fix $@
 
 tkfx-$(XIDGZ): fx-$(XIDGZ) tweakfile.txt
 	python -m tweakalign $(KEEPX) -i $< -M tweakfile.txt  -o $@
+
+ztkfx-$(XIDGZ): tkfx-$(XIDGZ)
+	python -m fixfasta $(KEEPX) --rmgapcols -i $< -o $@
 
 sk-$(XIDGZ): $(INITALIGN)
 	python -m xalign $(KEEPX) --dedash -i $(INITALIGN) -o $@
@@ -46,6 +53,6 @@ ztkfx-sk-$(XIDGZ): tkfx-sk-$(XIDGZ)
 
 Latest: Latest.ipkl
 
-Latest.ipkl: ztkfx-sk-$(XIDGZ)
+Latest.ipkl: $(.DEFAULT_GOAL)
 	python -m fixfasta $(KEEPX) --random -i $< -o $@
 
