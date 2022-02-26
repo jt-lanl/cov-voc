fe3dae5ffe8298bd4d10065af9596d6a31a92871
jt <jt@lanl.gov>
2022-02-26 10:05:52 -0700

    total re-write using class instead of module

--- verbose.py
+++ verbose.py
@@ -1,76 +1,127 @@
 '''verbose: print statements that depend on a user-set level of verbosity'''
-
+## an alternative implementation using class instead of just module
 import sys
 from collections import Counter
 
-## Global variable (well, global to the verbose module)
-VERBOSE=0
-
-def verbosity(vlevel):
-    '''user sets the level of verbosity'''
-    global VERBOSE
-    VERBOSE = vlevel
-
-def vnprint(vlevel,*p,**kw):
+class verbose:
     '''
-    if verbosity level is n or higher, then print
-    to sys.stderr and flush every print
+    encapsulates vprint functions
+    that write messages based on user-set level of verbosity
+    Typical use:
+       from verbose import verbose as v
+       ...
+       v.verbosity(1)
+       ...
+       v.vprint("read",n_sequences,"seqeunces from file")
+       ...
+    As an alternative to:
+       verbosity_level=1
+       ...
+       if verbosity_level > 0:
+           print("read",n_sequences,"sequences from file")
+       ...
     '''
-    if VERBOSE >= vlevel:
-        print(*p,file=sys.stderr,flush=True,**kw)
 
-def vprint(*p,**kw):  vnprint(1,*p,**kw)
-def vvprint(*p,**kw): vnprint(2,*p,**kw)
+    ## class variables
+    verbose_level=0
+    count=Counter()
 
-## Variant of vprint that only prints the warning a fixed
-## maximum number of times; thus warning you that something
-## is happening, but not scrolling pages and pages of it by
-## your screen.  Later, ie outside the loop you can (optionally)
-## call for a summary of how many times the warning was triggered.
+    @classmethod
+    def verbosity(cls,vlevel):
+        '''user sets the level of verbosity'''
+        cls.verbose_level = vlevel
 
-## common usage might be:
-## for data_sample in data:
-##    if is_bad(data_sample):
-##         vprint_only(5,'Bad data',data_sample.info)
-##    ...
-## vprint_only_summary('Bad data')
+    @classmethod
+    def vnprint(cls,vlevel,*p,**kw):
+        '''
+        if verbosity level is n or higher, then print
+        to sys.stderr and flush every print
+        '''
+        if cls.verbose_level >= vlevel:
+            print(*p,file=sys.stderr,flush=True,**kw)
 
-## Note that the warning string is a key.  That means you
-## can have several of these going at the saime time, identifying
-## different potential causes for a warning.  But it also means
-## that you cannot put changing information into that key string
+    @classmethod
+    def vprint(cls,*p,**kw):
+        '''verbose print'''
+        cls.vnprint(1,*p,**kw)
 
-## Okay:
-## for data_sample in data:
-##     if is_bad(data_sample):
-##         vprint_only(5,'Bad data',data_sample.info)
-##     if too_big(data_sample):
-##         vprint_only(5,'Too big','Data sample has',data_sample.size,'units'))
-##    ...
-## vprint_only_summary('Bad data')
-## vprint_only_summary('Too big')
+    @classmethod
+    def vvprint(cls,*p,**kw):
+        '''very verbose print'''
+        cls.vnprint(2,*p,**kw)
 
-## Mistake:
-##    vprint_only(5,f'Bad data: {data_sample.info}')
-##
+    ## Variant of vprint that only prints the warning a fixed
+    ## maximum number of times; thus warning you that something
+    ## is happening, but not scrolling pages and pages of it by
+    ## your screen.  Later, ie outside the loop you can (optionally)
+    ## call for a summary of how many times the warning was triggered.
 
-def vprint_only_summary(msg):
-    '''summarize how many times vprint_only was called with msg as first arguemnt'''
-    vprint_only(None,msg)
+    ## common usage might be:
+    ## from verbose import verbose as v
+    ## ...
+    ## for data_sample in data:
+    ##    if is_bad(data_sample):
+    ##         v.vprint_only(5,'Bad data',data_sample.info)
+    ##    ...
+    ## v.vprint_only_summary('Bad data')
 
-def vprint_only(maxcount,msg,*p,**kw):
-    '''vprint, but only maxcount times, at most'''
-    try:
-        vprint_only.count[msg] += 1
-    except AttributeError:
-        vprint_only.count = Counter()
-        vprint_only.count[msg] += 1
+    ## Note that the warning string is a key.  That means you
+    ## can have several of these going at the saime time, identifying
+    ## different potential causes for a warning.  But it also means
+    ## that you cannot put changing information into that key string
 
-    if maxcount is None:
-        vprint_only.count[msg] -= 1 # to un-count the call with None
-        if vprint_only.count[msg] > 0:
-            vprint(msg,vprint_only.count[msg],"warnings")
-    elif vprint_only.count[msg] < maxcount:
-        vprint(msg,*p,*kw)
+    ## Okay:
+    ## for data_sample in data:
+    ##     if is_bad(data_sample):
+    ##         v.vprint_only(5,'Bad data',data_sample.info)
+    ##     if too_big(data_sample):
+    ##         v.vprint_only(5,'Too big','Data sample has',data_sample.size,'units'))
+    ##    ...
+    ## v.vprint_only_summary('Bad data')
+    ## v.vprint_only_summary('Too big')
 
-## should there also be a vvprint_only ???
+    ## Mistake:
+    ##    vprint_only(5,f'Bad data: {data_sample.info}')
+    ##
+
+    @classmethod
+    def vnprint_only_summary(cls,vlevel,msg,*p,**kw):
+        '''
+        summarize how many times vprint_only was called
+        with msg as first arguemnt
+        '''
+        if cls.count[msg]:
+            cls.vnprint(vlevel,msg,cls.count[msg],*p,**kw)
+
+    @classmethod
+    def vnprint_only(cls,vlevel,maxcount,msg,*p,**kw):
+        '''vprint, but only maxcount times, at most'''
+        cls.count[msg] += 1
+        if cls.count[msg] <= maxcount:
+            cls.vnprint(vlevel,msg,*p,*kw)
+
+    @classmethod
+    def vprint_only(cls,maxcount,msg,*p,**kw):
+        '''vprint, but only maxcount times, at most'''
+        cls.vnprint_only(1,maxcount,msg,*p,**kw)
+
+    @classmethod
+    def vprint_only_summary(cls,msg,*p,**kw):
+        '''
+        summarize how many times vprint_only was called
+        with msg as first arguemnt
+        '''
+        cls.vnprint_only_summary(1,msg,*p,**kw)
+
+    @classmethod
+    def vvprint_only(cls,maxcount,msg,*p,**kw):
+        '''vprint, but only maxcount times, at most'''
+        cls.vnprint_only(2,maxcount,msg,*p,**kw)
+
+    @classmethod
+    def vvprint_only_summary(cls,msg,*p,**kw):
+        '''
+        summarize how many times vprint_only was called
+        with msg as first arguemnt
+        '''
+        cls.vnprint_only_summary(2,msg,*p,**kw)
