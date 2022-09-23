9babe75c237758f8521aa27279e43f7bdc132b13
jt <jt@lanl.gov>
2022-09-23 17:57:40 -0600

    uses parallel with tweakalign, and compresses only at the end

--- realign.mk
+++ realign.mk
@@ -5,7 +5,7 @@
 ## make -j3 -f src/realign.mk ID=20211024  makes re-aligned sequences based on date 20211024
 ## make -f src/realign.mk Latest           makes a new Latest.ipkl
 
-ID := 20220826
+ID := 20220907
 INITALIGN := data/REG_COMP.SPIKE.protein.Human.$(ID).fasta.xz
 KEEPX := --keepx
 ifeq ($(KEEPX),--keepx)
@@ -13,25 +13,48 @@ XID := xx$(ID)
 else
 XID := $(ID)
 endif
-XIDGZ := $(XID).fasta.gz
+XIDF := $(XID).fasta
+XIDGZ := $(XID).fasta.xz
 
-.DEFAULT_GOAL := ztkfx-$(XIDGZ)
+.DEFAULT_GOAL := all
 
-.PHONY: Latest
+.PHONY: all clean compress Latest
 
-fx-$(XIDGZ): $(INITALIGN)
+fx-$(XIDF): $(INITALIGN)
 	python -m fixalign $(KEEPX) -i $(INITALIGN) --fix $@
 
-tweak-$(XID).out: fx-$(XIDGZ)
+tweak-$(XID).out: fx-$(XIDF)
 	python -m matchfasta -i $< --showmut | perl -nle '/\b((.)(\d+)-,\+\3\2)\b/ and printf "[%s] []\n",$$1' | sort | uniq > $@
 
-tkfx-$(XIDGZ): fx-$(XIDGZ) tweak-$(XID).out
-	python -m tweakalign -M tweak-$(XID).out -M . $(KEEPX) -i fx-$(XIDGZ)  -o $@
+tkfx-$(XIDF): fx-$(XIDF) tweak-$(XID).out
+	parallel --header 2 -L2 --blocksize 400M --pipe \
+	python -m tweakalign -M tweak-$(XID).out -M . $(KEEPX) -i - -o - < fx-$(XIDF) > $@
 
-ztkfx-$(XIDGZ): tkfx-$(XIDGZ)
-	python -m fixfasta $(KEEPX) --rmgapcols -i $< -o $@
+ztkfx-$(XIDF): tkfx-$(XIDF)
+	python -m fixfasta $(KEEPX) --rmgapcols --random -i $< -o $@
 
-Latest: Latest.ipkl
+Latest: Latest.fasta
 
-Latest.ipkl: $(.DEFAULT_GOAL)
-	python -m fixfasta $(KEEPX) --random -i $< -o $@
+Latest.fasta: ztkfx-$(XIDF)
+	cp $< $@
+
+## if done, compress intermediate fastas
+
+fx-$(XIDGZ): fx-$(XIDF) tkfx-$(XIDF)
+	xz -T8 $<
+
+tkfx-$(XIDGZ): tkfx-$(XIDF) ztkfx-$(XIDF)
+	xz -T8 $<
+
+ztkfx-$(XIDGZ): ztkfx-$(XIDF) Latest.fasta
+	xz -T8 $< 
+
+compress: fx-$(XIDGZ) tkfx-$(XIDGZ) ztkfx-$(XIDGZ)
+
+clean:
+	/bin/rm -f fx-$(XIDF)       fx-$(XIDGZ)
+	/bin/rm -f tkfx-$(XIDF)   tkfx-$(XIDGZ)
+	/bin/rm -f ztkfx-$(XIDF) ztkfx-$(XIDGZ)
+	/bin/rm -f tweak-$(XID).out
+
+all: Latest.fasta compress
