cdf977926cc2fc8790d5818bdc77733059964d7f
James Theiler <jt@lanl.gov>
2024-03-05 22:14:12 -0700

    now uses both site-based and index-based tweaks

--- tweak.py
+++ tweak.py
@@ -2,9 +2,20 @@
 
 import re
 import itertools as it
+from functools import lru_cache
+import warnings
 import verbose as v
 import xopen
 import intlist
+import sequtil
+import mutant
+import mstringfix
+
+_DEDASH = re.compile("-")
+@lru_cache(maxsize=None)
+def de_gap(seq):
+    '''remove '-'s from sequence string'''
+    return _DEDASH.sub("",seq)
 
 class IndexTweak():
     '''pair of inconsistent subsequnces,
@@ -50,13 +61,21 @@ class IndexTweak():
                     v.print(f'In file={file}, invalid line: [{fullline}]')
         return tweaklist
 
+    @classmethod
+    def tweaks_from_filelist(cls,filelist):
+        '''return tweaklist by reading multiple files'''
+        tweaklist = []
+        for file in filelist or []:
+            tweaklist.extend( cls.tweaks_from_file(file) )
+        return tweaklist
+
     def is_trim(self):
         '''return True if sa and sb cannot be trimmed'''
         return self.sa[0]!=self.sb[0] and self.sa[-1]!=self.sb[-1]
 
     def __str__(self):
         '''simple output string, does not include counts'''
-        if hasattr(self,'ma'):
+        if hasattr(self,'ma') and self.ma is not None:
             ## if mstrings are available, prefer them for printing out
             return f'{self.ma} {self.mb}'
         return (f'{self.ndxlo} {self.ndxhi} '
@@ -161,3 +180,162 @@ class IndexTweak():
         lines.append( f' B: {self.sb} {mb} {cb}' )
         lines.append( f' A: {self.sa} {ma} {ca}' )
         return "\n".join(lines)
+
+########## SITE-BASED TWEAKS (mstring pairs)
+
+def get_mstring_pairs(mfiles,mpairs):
+    '''
+    Return a list of 2-tuples of m-strings;
+    Each pair has a from_mstring and a to_mstring
+    '''
+    mstringpairs = []
+
+    ## first: read the file(s) indicated by '-M file'
+    ## A file named '.' indicates that one should use the defaults
+    ## (which are currently listed in mstringfix.py)
+    ## Note that '-M' can be invoked more than once
+    ##   That is: '-M file1 -M file2' is okay
+    ##   Invalid: '-M file1 file2'
+    for mfile in mfiles or []:
+        if mfile == '.':
+            mfile = None
+        mstringpairs.extend( mstringfix.read_mstring_pairs(mfile) )
+
+    ## second: read from the strings on the command line
+    ## these are of the from -m from_mstring to_mstring
+    ## and multiple invocations of '-m' are allowed
+    for mspair in mpairs or []:
+        assert len(mspair)==2
+        mstringpairs.append(mspair)
+
+    ## ensure mstrings have brackets around them
+    mstringpairs = [ (mstringfix.mstring_brackets(a),
+                      mstringfix.mstring_brackets(b))
+                     for a,b in mstringpairs ]
+
+    ## are there any duplicates? remove them
+    mstringpairs = list(dict.fromkeys(mstringpairs))
+
+    ## Now we have all of our mstrings
+    for ma,mb in mstringpairs:
+        v.vvprint(f'{ma} -> {mb}')
+
+    return mstringpairs
+
+def get_extra_chars(mstringpairs):
+    '''
+    Characterize extra chars:
+    xtras[site] = number of extra chars after site
+    Add xtra chars for all the +nnnABC mstring component
+    '''
+    xtras = dict()
+    for mspair in mstringpairs:
+        for mstring in mspair:
+            for ssm in mutant.Mutation.from_mstring(mstring):
+                if ssm.ref == "+":
+                    xtras[ssm.site] = max( [xtras.get(ssm.site,0),
+                                            len(ssm.mut)] )
+    return xtras
+
+def add_extra_dashes(seqs,xxtras):
+    '''xxtras is dict keyed by indices of s.seq strings;
+       for each of those strings, we expand by extra dashes
+    '''
+    for s in seqs:
+        sseq = list(s.seq)
+        for ndx in xxtras:
+            sseq[ndx] += "-"*xxtras[ndx]
+        s.seq = "".join(sseq)
+        yield s
+
+def mstrings_to_ndx_seqs(mut_mgr,mstring_a,mstring_b):
+    '''
+    convert mstrings into short sequence-alignment fragments
+    along with the indices of where those sequences start/end
+    '''
+    mut_a = mutant.Mutation.from_mstring(mstring_a)
+    mut_b = mutant.Mutation.from_mstring(mstring_b)
+
+    sites = sorted(set(ssm.site for ssm in it.chain(mut_a,mut_b)))
+    lo,hi = sites[0],sites[-1]+1
+    ndxlo = min(mut_mgr.indices_from_site(lo))
+    ndxhi = max(mut_mgr.indices_from_site(hi-1))+1
+
+    seq_r = mut_mgr.refseq
+    seq_a = mut_mgr.seq_from_mutation(mut_a)
+    seq_b = mut_mgr.seq_from_mutation(mut_b)
+
+    seq_r = seq_r[ndxlo:ndxhi]
+    seq_a = seq_a[ndxlo:ndxhi]
+    seq_b = seq_b[ndxlo:ndxhi]
+
+    v.vvprint(f'{mstring_a}->{mstring_b}:')
+    v.vvprint(f'   r: {seq_r}')
+    v.vvprint(f'   a: {seq_a}')
+    v.vvprint(f'   b: {seq_b}')
+
+    ## None of these should happen
+    if seq_r == seq_a:
+        warnings.warn(f"Edit {mstring_a} is same as ref sequence; "
+                      "Are you sure you wan to change it !?")
+    if seq_a == seq_b:
+        v.vprint(f"Edit {mstring_a}->{mstring_b} will do nothing!")
+    if de_gap(seq_a) != de_gap(seq_b):
+        v.print(".".join(a+b for a,b in zip(de_gap(seq_a),de_gap(seq_b))))
+        v.print(f'   r: {seq_r}')
+        v.print(f'   a: {seq_a}')
+        v.print(f'   b: {seq_b}')
+        warnings.warn(f"Edit {mstring_a}->{mstring_b} "
+                      "will change actual sequence!"
+                      " not just the alignment\n"
+                      f"  {seq_a}->{seq_b}")
+        ## this shouldn't happen...but if it does, then don't do any replacing
+        seq_b = seq_a
+
+    return ndxlo,ndxhi,seq_a,seq_b
+
+def tweak_from_mstringpair(mut_mgr,mstring_a,mstring_b):
+    '''
+    convert mstring pair into IndexTweak object
+    '''
+    tweak_tuple = mstrings_to_ndx_seqs(mut_mgr,mstring_a,mstring_b)
+    tweak = IndexTweak(*tweak_tuple)
+    ## add ma,mb to tweak object ... in an unclean way
+    tweak.ma = mstring_a
+    tweak.mb = mstring_b
+    v.vprint('tweak:',str(tweak))
+    return tweak
+
+def add_needed_dashes(seqs,mstringpairs):
+    '''check if mstring pairs will require extra room in the sequences;
+    and if so, add the needed dashes to the sequences'''
+
+    if not mstringpairs:
+        return seqs
+
+    ## Identify where extra columns are needed (theoretical)
+    xtras = get_extra_chars(mstringpairs)
+    v.vprint("xtras:",xtras)
+
+    first,seqs = sequtil.get_first_item(seqs,keepfirst=True)
+    mut_mgr = mutant.MutationManager(first.seq)
+
+    ## Identify actual columns that need to be added
+    xxtras = dict() ## xx is ndx based instead of site based
+    for site in sorted(xtras):
+        ## determine how many extra columns there already are
+        mm_xtras = len(mut_mgr.indices_from_site(site))-1
+        need_xtras = xtras[site] - mm_xtras
+        if need_xtras > 0:
+            ## we'll need to add some columns
+            v.vprint("xtras:",site,mm_xtras,"->",xtras[site])
+            ## ndx is the index of the column to whch new columns should be added
+            ## should we use ndx+xtras[site] instead so we are adding columns
+            ## "after" the ones that are alrady there??? just thinking out loud
+            ndx = mut_mgr.index_from_site(site) ## hmm,adding from left side??
+            xxtras[ndx] = need_xtras
+
+    if len(xxtras):
+        seqs = add_extra_dashes(seqs,xxtras)
+
+    return seqs,xxtras
