c4e115ce1ba52d9281deca2bbbebb03d69a9dd8c
James Theiler <jt@lanl.gov>
2024-03-24 18:23:05 -0600

    major overhaul to use new coding infrastructure

--- mut2fasta.py
+++ mut2fasta.py
@@ -1,103 +1,93 @@
-DESCRIPTION='''
-mut2fasta: takes one or more mutant strings (either from a file or from the command line), 
-each of which looks something like "[A222V,A262S,S494P,D614G]", and produces a fasta file, 
-each sequence of which corresponds to a mutant specified by the string, relative to the 
-reference sequence, which is the first sequence in the specified reference fasta file.
 '''
-import sys
+takes one or more mutant strings (either from a file or from the
+command line), each of which looks something like
+"[A222V,A262S,S494P,D614G]", and produces a fasta file, each sequence
+of which corresponds to a mutant specified by the string, relative to
+the reference sequence, which is the first sequence in the specified
+reference fasta file.
+'''
+
 import re
-import warnings
 import argparse
 
-import readseq
+import verbose as v
+import xopen
+import sequtil
+import wildtypes
 import mutant
+import tweak as tku
 
-def getargs():
-    ap = argparse.ArgumentParser(description=DESCRIPTION)
+def _getargs():
+    ap = argparse.ArgumentParser(description=__doc__)
     paa = ap.add_argument
-    paa("--input","-i",
-        help="input fasta file with reference sequence first")
-    paa("--skipref",action="store_true",
-        help="do not include reference sequence in fasta output file")
-    paa("--mutstrings","-m",nargs='+',
-        help="one or more strings of the form '[L5F,...,Q957R]'")
-    paa("--mutfile",
-        help="input file with list of mutant strings")
-    paa("--nodash",action="store_true",
-        help="remove dashes from the output sequences")
-    paa("--output","-o",
-        help="ouptut fasta file of mutated seqeunces")
     paa("--verbose","-v",action="count",default=0,
         help="verbose")
+    paa = ap.add_argument_group("Mutation to Sequence Options").add_argument
+    paa("--mstrings","-m",nargs='+',
+        help="one or more strings of the form '[L5F,...,Q957R]'")
+    paa("--mfile","-M",
+        help="input file with list of mutant m-strings")
+    paa("--nodash",action="store_true",
+        help="remove dashes from the output sequences")
+    paa("--skipref",action="store_true",
+        help="Do not include reference sequence in output")
+    paa("--output","-o",
+        help="ouptut fasta file of mutated seqeunces")
     args = ap.parse_args()
     return args
 
-def rd_mutfile(filename):
-    with open(filename) as f:
-        for line in f:
-            line = re.sub("#.*","",line)
-            line = line.strip()
-            if not line:
-                continue
-            yield mutant.Mutation.from_mstring(line)
+def rd_mfile(filename):
+    '''read file with mstrings'''
+    with xopen.xopen(filename) as fin:
+        yield from xopen.nonempty_lines(fin)
 
-def main(args):
+def _main(args):
 
     def de_dash(s):
+        '''remove dashes from string'''
         if args.nodash:
             s.seq = re.sub("-","",s.seq)
         return s
 
-    mutlist = []
-    if args.mutfile:
-        mutlist.extend( rd_mutfile(args.mutfile) )
-    if args.mutstrings:
-        mutlist.extend( mutant.Mutation(mstring)
-                        for mstring in args.mutstrings)
+    mstringlist = []
+    if args.mfile:
+        mstringlist.extend( rd_mfile(args.mfile) )
+    if args.mstrings:
+        mstringlist.extend( args.mstrings )
 
-    vprint(len(mutlist),"mutant strings have been read")
-    for m in mutlist:
-        vvprint("  ",m)
+    v.vprint(f'Read {len(mstringlist)} m-strings')
+    for mstring in mstringlist:
+        v.vvprint("  ",mstring)
 
-    if len(mutlist) == 0:
-        raise RuntimeError("Must specify mutlist with either --mutfile for --mutstirngs or -m")
+    if not mstringlist:
+        v.print("Must specify mstrings on command line "
+                "with --mfile/-M and/or --mstrings/-m")
+        return
 
-    ## Grab the first sequence from a reference file
-    seqs = readseq.read_seqfile(args.input,maxseqs=1)
-    seqs = list(seqs)
-    wuhan = seqs[0].seq
+    ## Grab the Wuhan base sequence
+    wuhan = sequtil.SequenceSample( wildtypes.SEQUENCE_NAME,
+                                    wildtypes.SEQUENCE )
+    if xtras := tku.extra_chars_from_mstrings(mstringlist):
+        ## hack (works only becuz wuhan has no dashes, initially)
+        xpand = {s-1:x for s,x in xtras.items()}
+        wuhan.seq = tku.expand_seq(xpand,wuhan.seq)
 
-    #if "-" in wuhan:
-    #    warnings.warn("Dashes have been stripped from reference sequence")
-    #    wuhan = re.sub("-","",wuhan)
-    #    seqs[0].seq = wuhan
+    mut_mgr = mutant.MutationManager(wuhan.seq)
 
-    MM = mutant.MutationManager(wuhan)
-    
     mutseqs = []
     if not args.skipref:
-        mutseqs.append(seqs[0])
-    for mut in mutlist:
-        mutseq = MM.seq_from_mutation(mut)
-        mutseqs.append( readseq.SequenceSample( str(mut), mutseq ) )
+        mutseqs.append(wuhan)
+    for mstring in mstringlist:
+        mutseq = mut_mgr.regex_from_mstring(mstring,exact=True)
+        mutseqs.append( sequtil.SequenceSample( mstring, mutseq ) )
 
-    mutseqs = (de_dash(s) for s in mutseqs)        
+    mutseqs = [de_dash(s) for s in mutseqs]
 
     if args.output:
-        readseq.write_seqfile(args.output,mutseqs)
-    
-    
+        sequtil.write_seqfile(args.output,mutseqs)
 
 if __name__ == "__main__":
 
-    args = getargs()
-    def vprint(*p,**kw):
-        if args.verbose:
-            print(*p,file=sys.stderr,flush=True,**kw)
-    def vvprint(*p,**kw):
-        if args.verbose>1:
-            print(*p,file=sys.stderr,flush=True,**kw)
-
-    main(args)
-    
-
+    _args = _getargs()
+    v.verbosity(_args.verbose)
+    _main(_args)
