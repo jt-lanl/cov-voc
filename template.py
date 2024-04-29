06773fc475367bbdf54b2b52c6f075032e4481a7
James Theiler <jt@lanl.gov>
2024-04-28 20:31:01 -0600

    1/ added breakpipe; 2/ reformatted in black style

--- template.py
+++ template.py
@@ -1,21 +1,25 @@
-'''Template doc string'''
+"""Template doc string"""
 
 import argparse
 import verbose as v
+import breakpipe
+
 
 def _getargs():
-    '''parse options from command line'''
+    """parse options from command line"""
     argparser = argparse.ArgumentParser(description=__doc__)
     generic_paa = argparser.add_argument
-    generic_paa("--verbose","-v",action="count",default=0,
-                help="verbose")
-    paa = argparser.add_argument_group('Program Options').add_argument
+    generic_paa("--verbose", "-v", action="count", default=0, help="verbose")
+    paa = argparser.add_argument_group("Program Options").add_argument
     args = argparser.parse_args()
     return args
 
+
+@breakpipe.no_broken_pipe
 def _main(args):
-    '''main'''
+    """main"""
     v.vprint(args)
+
 
 if __name__ == "__main__":
 
